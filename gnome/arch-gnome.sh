902f9bfc430af1cbabff1f2f030a55a8ff16b8d4
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2021-12-22 13:29:55 +0100

    a

--- gnome/arch-gnome.sh
+++ gnome/arch-gnome.sh
@@ -108,6 +108,13 @@ yay -S wmctrl xdotool libinput-gestures gestures
 cd ~/.config/
 wget https://github.com/gsingh704/mysetup/raw/main/dotfiles/libinput-gestures.conf
 libinput-gestures-setup autostart start
+----------------------------------------------------
+#Insall touchegg and touch 
+yay -S touchegg touchegg
+sudo systemctl enable touchegg.service  #enble and start the service
+sudo systemctl start touchegg
+
+
 -------------------------------------
 gvfs-goa gvfs-google
 -------------------------------------
