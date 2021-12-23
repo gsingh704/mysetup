9424305c7c17ae88f8c94b8a0af3cbd4ee3eee7d
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2021-12-23 14:46:46 +0100

    j

--- other/loop.sh
+++ other/loop.sh
@@ -3,9 +3,6 @@ i=0
 while [ $i -ne $1 ]
 do
         i=$(($i+1))
-
         mkdir -p user$i/docs/news  user$i/docs/old  user$i/docs/future  user$i/works   user$i/backup
         touch  user$i/docs/news/1.txt     user$i/docs/news/2.txt
-
-
 done
