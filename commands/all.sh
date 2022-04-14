41f7ebc272361eb24280b0bbaf19e89e7988d8d5
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2022-04-14 23:11:46 +0200

    Update all.sh

--- commands/all.sh
+++ commands/all.sh
@@ -14,10 +14,8 @@ exit
 #
 sudo pacman -Suy
 sudo pacman -S gnome-themes-extra  firefox  wget git base-devel nodejs  npm nvidia nvidia-dkms nvidia-utils nvidia-prime  mesa      bluez bluez-utils   #lib32-nvidia-utils
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-yay -Syu ruby-fusuma  ruby-fusuma-plugin-sendkey  pipewire-pulse pipewire-alsa qgnomeplatform chrome-gnome-shell asusctl supergfxctl
+
+yay -Syu ruby-fusuma  ruby-fusuma-plugin-sendkey adw-gtk3 pipewire-pulse pipewire-alsa qgnomeplatform chrome-gnome-shell asusctl supergfxctl
 sudo gpasswd -a $USER input
 sudo systemctl enable --now power-profiles-daemon.service
 sudo systemctl enable --now supergfxd.service
