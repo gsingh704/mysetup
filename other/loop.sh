4f0127181fc4a2ba5f6ed3e3854c0564e28af74b
Gurjant Singh <gurjant.singh@cirvianum.cat>
2022-12-09 23:48:21 +0100

    ok

--- other/loop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-i=0
-while [ $i -ne $1 ]
-do
-        i=$(($i+1))
-        mkdir -p user$i/docs/news  user$i/docs/old  user$i/docs/future  user$i/works   user$i/backup
-        touch  user$i/docs/news/1.txt     user$i/docs/news/2.txt
-done
