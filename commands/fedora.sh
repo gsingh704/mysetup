7655b4ff5cddaeeded13c62376134e4984eac6d9
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2023-04-11 22:45:53 +0200

    ok

--- commands/fedora.sh
+++ commands/fedora.sh
@@ -73,4 +73,4 @@ SELINUXTYPE=targeted' > /etc/sysconfig/selinux "
 #better video
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install ffmpeg
+dnf swap ffmpeg-free ffmpeg --allowerasing
