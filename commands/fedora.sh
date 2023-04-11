4e17a8c6b4c39135ff04f66e9f4f5f3fd9388452
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2023-04-11 21:15:46 +0200

    ok

--- commands/fedora.sh
+++ commands/fedora.sh
@@ -70,3 +70,8 @@ sudo sh -c "echo 'SELINUX=disabled
 SELINUXTYPE=targeted' > /etc/sysconfig/selinux "
 
 
+#better video
+sudo dnf update -y
+sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install -y lame\* --exclude=lame-devel
+sudo dnf group upgrade --with-optional -y Multimedia
