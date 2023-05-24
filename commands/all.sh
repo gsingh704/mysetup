a33f5df32e2dcbe55162de6dff8c5713bc4e028d
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2023-05-24 21:13:49 +0200

    ok

--- commands/all.sh
+++ commands/all.sh
@@ -1,18 +1,32 @@
-#
+# make firefox run wayland ###################################################################
 sudo sh -c "echo 'MOZ_ENABLE_WAYLAND=1
 MOZ_USE_XINPUT2=1
 MOZ_X11_EGL=1
 QT_STYLE_OVERRIDE=adwaita-dark' > /etc/environment"
 
 
-#
-sudo pacman -Syu --noconfirm   zsh firefox  wget git base-devel nvidia  nvidia-utils nvidia-prime  vulkan-radeon bluez bluez-utils lib32-nvidia-utils gvfs-google  
-yay -Syu --noconfirm ruby-fusuma  ruby-fusuma-plugin-sendkey adw-gtk3 adwaita-qt5 pipewire-pulse pipewire-alsa   asusctl supergfxctl
-sudo gpasswd -a $USER input
+# general ####################################################################################
+yay -Syu --noconfirm  gvfs-google adw-gtk3 adwaita-qt5 
+#sudo systemctl enable --now bluetooth
+
+#graphics related ############################################################################
+yay -Syu --noconfirm  base-devel nvidia  nvidia-utils nvidia-prime  vulkan-radeon lib32-nvidia-utils asusctl supergfxctl 
 sudo systemctl start asusd.service
 sudo systemctl enable --now supergfxd.service
-sudo systemctl enable --now bluetooth
- 
+sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
+
+
+asusctl fan-curve -m quiet -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f cpu
+asusctl fan-curve -m quiet -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
+asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f cpu
+asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
+asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f cpu
+asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f gpu
+
+#touchpad gestures ###########################################################################
+yay -Syu --noconfirm ruby-fusuma  ruby-fusuma-plugin-sendkey
+sudo gpasswd -a $USER input
+
 mkdir ~/.config/autostart/
 echo "[Desktop Entry]    
 Type=Application
@@ -20,7 +34,6 @@ Exec=fusuma
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true" > ~/.config/autostart/fusuma.desktop
-
 
 mkdir -p ~/.config/fusuma
 cd ~/.config/fusuma
@@ -32,17 +45,9 @@ wget https://github.com/gsingh704/mysetup/raw/main/dotfiles/gnome/extensions.zip
 unzip extensions.zip
 wget https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/gnome/dconf-settings-ft.ini
 
-#fish
+#fish ########################################################################################
+yay -Syu --noconfirm fish
 mkdir ~/.config/fish
 cd ~/.config/fish
 wget https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/config.zsh
 
-sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
-
-#reboot    
-asusctl fan-curve -m quiet -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f cpu
-asusctl fan-curve -m quiet -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
-asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f cpu
-asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
-asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f cpu
-asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f gpu
