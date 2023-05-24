e72f75da4475c60c01f2ba4cc6f59d949f21b5a1
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2023-05-24 21:54:47 +0200

    ok

--- commands/all.sh
+++ commands/all.sh
@@ -11,9 +11,11 @@ mkdir -p .themes/my/gnome-shell && echo '.clock {
     border-width: 0;
  }' > .themes/my/gnome-shell/gnome-shell.css
 
+
 # general ####################################################################################
 yay -Syu --noconfirm visual-studio-code-bin gvfs-google adwaita-qt5 
 #sudo systemctl enable --now bluetooth
+
 
 #graphics related ############################################################################
 yay -Syu --noconfirm  base-devel nvidia  nvidia-utils nvidia-prime  vulkan-radeon lib32-nvidia-utils asusctl supergfxctl 
@@ -21,13 +23,13 @@ sudo systemctl start asusd.service
 sudo systemctl enable --now supergfxd.service
 sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
 
-
 asusctl fan-curve -m quiet -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f cpu
 asusctl fan-curve -m quiet -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
 asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f cpu
 asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
 asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f cpu
 asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f gpu
+
 
 #touchpad gestures ###########################################################################
 yay -Syu --noconfirm ruby-fusuma  ruby-fusuma-plugin-sendkey
@@ -45,11 +47,14 @@ mkdir -p ~/.config/fusuma
 cd ~/.config/fusuma
 wget https://github.com/gsingh704/mysetup/raw/main/dotfiles/gestures/config.yml
 
+
+# gnome dconf ################################################################################
 cd ~/.local/share/gnome-shell
 rm -rf extensions
 wget https://github.com/gsingh704/mysetup/raw/main/dotfiles/gnome/extensions.zip
 unzip extensions.zip
 wget https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/gnome/dconf-settings-ft.ini
+
 
 #fish ########################################################################################
 yay -Syu --noconfirm fish
