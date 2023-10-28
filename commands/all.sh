746be48e0e3a5371f30fc749c63992c5b40a00a0
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2023-10-28 20:26:22 +0200

    ok

--- commands/all.sh
+++ commands/all.sh
@@ -2,7 +2,7 @@
 sudo sh -c "echo 'MOZ_ENABLE_WAYLAND=1
 MOZ_USE_XINPUT2=1
 MOZ_X11_EGL=1
-QT_STYLE_OVERRIDE=adwaita-dark' > /etc/environment"
+#QT_STYLE_OVERRIDE=adwaita-dark' > /etc/environment"
 
 
 # general ####################################################################################
@@ -23,7 +23,7 @@ asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:
 asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
 asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f cpu
 asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f gpu
-
+asusctl fan-curve -m balanced -e true   
 
 #touchpad gestures ###########################################################################
 yay -Syu --noconfirm wget ruby-fusuma ruby-fusuma-plugin-sendkey
@@ -48,7 +48,7 @@ cd ~/.local/share/gnome-shell
 # rm -rf extensions
 # wget https://github.com/gsingh704/mysetup/raw/main/dotfiles/gnome/extensions.zip
 # unzip extensions.zip
-# wget https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/gnome/dconf-settings-ft.ini
+wget https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/gnome/dconf-settings-ft.ini
 
 mkdir -p ~/.themes/my/gnome-shell && echo '.clock {
     border-width: 0;
