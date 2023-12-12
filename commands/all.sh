45fb240319fa7978e1f0f983584489324b47b75f
GURJANT SINGH <85999340+gsingh704@users.noreply.github.com>
2023-12-12 17:02:17 +0100

    1

--- commands/all.sh
+++ commands/all.sh
@@ -4,17 +4,12 @@ MOZ_USE_XINPUT2=1
 MOZ_X11_EGL=1
 #QT_STYLE_OVERRIDE=adwaita-dark' > /etc/environment"
 
-
-# general ####################################################################################
-yay -Syu --noconfirm visual-studio-code-bin  # gvfs-google adwaita-qt5 
-sudo systemctl enable --now bluetooth
-
-
 #graphics related ############################################################################
 yay -R --confirm nvidia-dkms
 yay -Syu --noconfirm  base-devel nvidia  nvidia-utils nvidia-prime  vulkan-radeon lib32-nvidia-utils asusctl supergfxctl 
 sudo systemctl start asusd
 sudo systemctl enable --now supergfxd.service
+sudo systemctl enable --now bluetooth
 sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
 
 asusctl fan-curve -m quiet -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f cpu
@@ -23,7 +18,7 @@ asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:
 asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
 asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f cpu
 asusctl fan-curve -m performance -D 30c:0,40c:0,50c:0,60c:10,70c:55,80c:90,90c:100,100c:100 -e true -f gpu
-asusctl fan-curve -m balanced -e true   
+asusctl fan-curve -m balanced -e true  
 
 # gnome dconf ################################################################################
 yay -Syu --noconfirm wget adw-gtk3 extension-manager
@@ -66,9 +61,8 @@ cd ~
 rm -f .zshrc*
 wget https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/.zshrc
 
-
 #docker ######################################################################################
-yay -Syu --noconfirm docker docker-compose 
+yay -Syu --noconfirm docker docker-compose visual-studio-code-bin  # gvfs-google adwaita-qt5 
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl enable --now docker 
