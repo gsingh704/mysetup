345daf81ef3b171489f30f841bf447c598b01524
Gurjant Singh <85999340+gsingh704@users.noreply.github.com>
2024-03-21 21:28:43 +0100

    1

--- commands/misc.sh
+++ commands/misc.sh
@@ -1,5 +1,28 @@
-# install netdata
-bash <(curl -Ss https://my-netdata.io/kickstart.sh)
+#Linux 
+efibootmgr -v
+sudo efibootmgr -b 0003 -B #example
+---------------------------------------------------------------
+#change kernel settings 
+sudoedit /etc/default/grub
+GRUB_CMDLINE_LINUX_DEFAULT="i8042.nopnp=1 pci=nocrs"
+sudo update-grub   *orororororor* sudo grub-mkconfig -o /boot/grub/grub.cfg
+reboot
+
+Pop os 
+sudo kernelstub -o "i8042.nopnp=1 pci=nocrs"
+----------------------------------------------------
+
+export QT_QPA_PLATFORMTHEME=qt5ct
+QT_SCALE_FACTOR=1.4
+----------------------------------------------
+
+#figerprint
+https://github.com/knauth/goodix-521d-explanation
+pysub
+
+
+###to run proton games from ntfs
+sudo ntfs-3g -o uid=1000,guid=1000 /dev/nvme0n1p6 /mnt/0E72E0784E3878AE
 
 #Gestures
 #Libinput-Gestures 
