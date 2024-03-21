6796f63a64be0c697cf5eab2e8e7c1494df7aea7
Gurjant Singh <85999340+gsingh704@users.noreply.github.com>
2024-03-21 21:03:03 +0100

    uodated for lebnovo

--- commands/all.sh
+++ commands/all.sh
@@ -1,28 +1,12 @@
-#graphics related ############################################################################
-yay -R --confirm nvidia-dkms
-yay -Syu --noconfirm  base-devel nvidia  nvidia-utils nvidia-prime  vulkan-radeon lib32-nvidia-utils asusctl supergfxctl 
-sudo systemctl start asusd
-sudo systemctl enable --now supergfxd.service
 sudo systemctl enable --now bluetooth
-sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
-
-asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f cpu
-asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
-asusctl fan-curve -m balanced -e true  
 
 # gnome dconf ################################################################################
 yay -Syu --noconfirm wget adw-gtk3 extension-manager
-wget -O ~/.local/share/gnome-shell/dconf-settings-ft.ini https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/gnome/dconf-settings-ft.ini
-wget -O ~/.local/share/gnome-shell/1.jpg https://w.wallhaven.cc/full/md/wallhaven-mdjrqy.jpg
+curl -o ~/.local/share/gnome-shell/dconf-settings-ft.ini https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/gnome/dconf-settings-ft.ini --create-dirs
+curl -o ~/.local/share/gnome-shell/1.jpg https://w.wallhaven.cc/full/md/wallhaven-mdjrqy.jpg --create-dirs
+curl -o ~/.local/share/fonts/RedHatDisplay-Bold.ttf https://raw.githubusercontent.com/RedHatOfficial/RedHatFont/master/fonts/proportional/static/ttf/RedHatDisplay-Bold.ttf --create-dirs
 
-# mkdir -p ~/.themes/my/gnome-shell && echo '.clock {
-#     border-width: 0;
-#  }' > ~/.themes/my/gnome-shell/gnome-shell.css
-
-mkdir ~/.local/share/fonts
-cd  ~/.local/share/fonts
-wget https://raw.githubusercontent.com/RedHatOfficial/RedHatFont/master/fonts/proportional/static/ttf/RedHatDisplay-Bold.ttf
-
+mkdir -p ~/.themes/my/gnome-shell && echo '.clock {border-width: 0;}' > ~/.themes/my/gnome-shell/gnome-shell.css
 #touchpad gestures ###########################################################################
 yay -Syu --noconfirm wget ruby-fusuma ruby-fusuma-plugin-sendkey
 sudo gpasswd -a $USER input
@@ -36,14 +20,12 @@ NoDisplay=false
 Name=fusuma
 X-GNOME-Autostart-enabled=true" > ~/.config/autostart/fusuma.desktop
 
-mkdir -p ~/.config/fusuma
-cd ~/.config/fusuma
-wget https://github.com/gsingh704/mysetup/raw/main/dotfiles/gestures/config.yml
+curl -o ~/.config/fusuma/config.yml https://github.com/gsingh704/mysetup/raw/main/dotfiles/gestures/config.yml --create-dirs
 
 #zsh ########################################################################################
 yay -Syu --noconfirm wget zsh git zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting
 chsh -s $(which zsh)
-wget -O ~/.zshrc https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/.zshrc
+curl -o ~/.zshrc https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/.zshrc
 
 #docker ######################################################################################
 yay -Syu --noconfirm docker docker-compose visual-studio-code-bin  # gvfs-google adwaita-qt5 
---
+++ commands/g14_arch.sh
@@ -0,0 +1,52 @@
+#graphics related ############################################################################
+yay -R --confirm nvidia-dkms
+yay -Syu --noconfirm  base-devel nvidia  nvidia-utils nvidia-prime  vulkan-radeon lib32-nvidia-utils asusctl supergfxctl 
+sudo systemctl start asusd
+sudo systemctl enable --now supergfxd.service
+sudo systemctl enable --now bluetooth
+sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
+
+asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f cpu
+asusctl fan-curve -m balanced -D 30c:0,40c:0,50c:0,60c:0,70c:0,80c:0,90c:0,100c:0  -e true -f gpu
+asusctl fan-curve -m balanced -e true  
+
+# gnome dconf ################################################################################
+yay -Syu --noconfirm wget adw-gtk3 extension-manager
+wget -O ~/.local/share/gnome-shell/dconf-settings-ft.ini https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/gnome/dconf-settings-ft.ini
+wget -O ~/.local/share/gnome-shell/1.jpg https://w.wallhaven.cc/full/md/wallhaven-mdjrqy.jpg
+
+# mkdir -p ~/.themes/my/gnome-shell && echo '.clock {
+#     border-width: 0;
+#  }' > ~/.themes/my/gnome-shell/gnome-shell.css
+
+mkdir ~/.local/share/fonts
+cd  ~/.local/share/fonts
+wget https://raw.githubusercontent.com/RedHatOfficial/RedHatFont/master/fonts/proportional/static/ttf/RedHatDisplay-Bold.ttf
+
+#touchpad gestures ###########################################################################
+yay -Syu --noconfirm wget ruby-fusuma ruby-fusuma-plugin-sendkey
+sudo gpasswd -a $USER input
+
+mkdir ~/.config/autostart/
+echo "[Desktop Entry]    
+Type=Application
+Exec=fusuma
+Hidden=false
+NoDisplay=false
+Name=fusuma
+X-GNOME-Autostart-enabled=true" > ~/.config/autostart/fusuma.desktop
+
+mkdir -p ~/.config/fusuma
+cd ~/.config/fusuma
+wget https://github.com/gsingh704/mysetup/raw/main/dotfiles/gestures/config.yml
+
+#zsh ########################################################################################
+yay -Syu --noconfirm wget zsh git zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting
+chsh -s $(which zsh)
+wget -O ~/.zshrc https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/.zshrc
+
+#docker ######################################################################################
+yay -Syu --noconfirm docker docker-compose visual-studio-code-bin  # gvfs-google adwaita-qt5 
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable --now docker 
