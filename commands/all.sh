1f08b355e8c0ed21aff5916561a3f5decc571aee
Gurjant Singh <85999340+gsingh704@users.noreply.github.com>
2024-10-15 17:47:37 +0200

    fusuma/dns

--- commands/all.sh
+++ commands/all.sh
@@ -3,6 +3,7 @@ sudo systemctl enable --now bluetooth
 echo '--enable-features=TouchpadOverscrollHistoryNavigation
 --ozone-platform=wayland' > ~/.config/chrome-flags.conf
 sudo sed -i '1i\[Element Master]\nswitch = mute\nvolume = ignore\n' /usr/share/alsa-card-profile/mixer/paths/analog-output.conf.common
+sudo sed -i '1s/^/nameserver 1.1.1.1\n/' /etc/resolv.conf
 
 
 # gnome dconf ################################################################################
@@ -15,13 +16,14 @@ curl -o ~/.local/share/fonts/RedHatDisplay-Bold.ttf https://raw.githubuserconten
 mkdir -p ~/.themes/my/gnome-shell && echo '.clock {border-width: 0;}' > ~/.themes/my/gnome-shell/gnome-shell.css
 
 #touchpad gestures ###########################################################################
-yay -Syu --noconfirm wget ruby-fusuma ruby-fusuma-plugin-sendkey
+yay -Syu --noconfirm wget ruby-fusuma
+gem install fusuma fusuma-plugin-sendkey 
 sudo gpasswd -a $USER input
 
 mkdir ~/.config/autostart/
 echo "[Desktop Entry]    
 Type=Application
-Exec=fusuma
+Exec=/home/j/.local/share/gem/ruby/3.3.0/bin/fusuma
 Hidden=false
 NoDisplay=false
 Name=fusuma
@@ -39,8 +41,10 @@ chsh -s $(which zsh)
 curl -o ~/.zshrc https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/sh/.zshrc
 
 #acestream
-curl -o ~/.local/share/applications/acestream.AppImage https://github.com/bro2020/acestream-appimage/releases/download/v2.2/AceStream-3.1.49-v2.2.AppImage
-chmod +x ~/.local/share/applications/acestream.AppImage
+# curl -o ~/.local/share/applications/acestream.AppImage https://github.com/bro2020/acestream-appimage/releases/download/v2.2/AceStream-3.1.49-v2.2.AppImage
+# chmod +x ~/.local/share/applications/acestream.AppImage
+yay -Syu --noconfirm  acestream-engine acestream-launcher
+
 
 #docker ######################################################################################
 yay -Syu --noconfirm docker docker-compose visual-studio-code-bin  # gvfs-google adwaita-qt5 
