9463443cb32caa20545cf04a05356cf9c72d21d2
Gurjant Singh <85999340+gsingh704@users.noreply.github.com>
2024-10-22 12:43:38 +0100

    removed gestures and made them commands

--- commands/common.sh
+++ commands/common.sh
@@ -9,19 +9,12 @@ sudo sed -i '1s/^/nameserver 1.1.1.1\n/' /etc/resolv.conf
 sudo sh -c "echo 'export ELECTRON_OZONE_PLATFORM_HINT=auto' >> /etc/environment"
 
 #zsh----------------------------------------------------
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"  #install oh-my-zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-
-echo 'export ZSH="/home/$USER/.oh-my-zsh"
-ZSH_THEME="jonathan"
-plugins=(git zsh-completions  zsh-completions zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)
-source $ZSH/oh-my-zsh.sh
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down' > ~/.zshrc
-
+echo -e 'export ZSH="/home/$USER/.oh-my-zsh"\nZSH_THEME="jonathan"\nplugins=(git zsh-completions zsh-completions zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)\nsource $ZSH/oh-my-zsh.sh\nbindkey "$terminfo[kcuu1]" history-substring-search-up\nbindkey "$terminfo[kcud1]" history-substring-search-down' > ~/.zshrc
 
 #gnome config------------------------------------------
 curl -o ~/.local/share/gnome-shell/dconf-settings-ft.ini https://raw.githubusercontent.com/gsingh704/mysetup/main/dotfiles/dconf-settings-ft.ini --create-dirs
@@ -36,17 +29,8 @@ gnome-extensions install -f gestureImprovements47@gestures.zip
 sudo gem install revdev bundler fusuma fusuma-plugin-sendkey
 sudo gpasswd -a $USER input
 
-mkdir ~/.config/autostart/
-echo "[Desktop Entry]    
-Type=Application
-Exec=fusuma
-Hidden=false
-NoDisplay=false
-X-GNOME-Autostart-enabled=true" > ~/.config/autostart/fusuma.desktop
-
-mkdir -p ~/.config/fusuma
-cd ~/.config/fusuma
-wget https://github.com/gsingh704/mysetup/raw/main/dotfiles/gestures/config.yml
+mkdir -p ~/.config/autostart && echo -e "[Desktop Entry]\nType=Application\nExec=fusuma\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true" > ~/.config/autostart/fusuma.desktop
+mkdir ~/.config/fusuma/ && echo -e "swipe:\n  4:\n    left:\n      sendkey: 'LEFTCTRL+TAB'\n    right:\n      sendkey: 'LEFTCTRL+LEFTSHIFT+TAB'\n    up:\n      sendkey: 'LEFTCTRL+LEFTSHIFT+T'\n    down:\n      sendkey: 'LEFTCTRL+W'\npinch:\n  3:\n    out:\n      sendkey: 'LEFTCTRL+T'\n    in:\n      sendkey: 'LEFTCTRL+T'\n\nthreshold:\n  swipe: 0.5\n  pinch: 0.2\n\ninterval:\n  swipe: 1.0\n  pinch: 0.7" >  ~/.config/fusuma
 
 #docker----------------------------------------
 sudo groupadd docker
